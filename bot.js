bed9fb57eb32e2617fb45137252a6f2be0c0c4f7
Nevylish <nevylish@nevylish.fr>
2023-04-03 21:49:20 +0200

    add functions types & interaction local names

--- bot.js
+++ bot.js
@@ -1,16 +1,36 @@
+/*
+ * UppercaseBot, finally use upper case letters for your channel names.
+ * Copyright (c) 2023 Nevylish <bonjour@nevylish.fr>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const {
     ChannelType,
     Client,
     GatewayIntentBits,
     Events,
     EmbedBuilder,
+    CommandInteraction,
+    GuildMember,
     Colors,
     WebhookClient,
     PermissionsBitField
 } = require("discord.js");
 
 class UpperCaseClient extends Client {
-    permissionsInteger = 277025442833;
+    // permissionsInteger = 277025442833;
 
     constructor(token) {
         super({
@@ -28,7 +48,11 @@ class UpperCaseClient extends Client {
         });
     }
 
-    // Commands
+    /**
+     * Create channel with alternatives uppercase letters
+     * @param {CommandInteraction} interaction
+     * @returns {void}
+     */
     async createChannel(interaction) {
         const channel_name = interaction?.options?.getString("channel_name");
         let channel_type = interaction?.options?.getString("channel_type");
@@ -61,9 +85,14 @@ class UpperCaseClient extends Client {
             });
     }
 
+    /**
+     * Rename targeted channel with alternatives uppercase letters
+     * @param {CommandInteraction} interaction
+     * @returns {void}
+     */
     async renameChannel(interaction) {
         const channel_selected = interaction?.options?.getChannel('channel');
-        const channel_name = interaction?.options?.getString('name');
+        const channel_name = interaction?.options?.getString('new_name');
 
         if (!this.isStaff(interaction.member)) {
             this.commandError(interaction, 'You do not have the necessary permissions to run this command.');
@@ -85,22 +114,21 @@ class UpperCaseClient extends Client {
             });
     }
 
-    // Base
+    /**
+     * Start events listerners
+     * @returns {void}
+     */
     eventsListeners() {
         this.on(Events.InteractionCreate, (interaction) => {
             if (!interaction.isCommand()) return;
 
             switch (interaction.commandName) {
                 case "create-channel": {
-                    this.createChannel(interaction).catch((err) =>
-                        this.commandError(interaction, err.message)
-                    );
+                    this.createChannel(interaction);
                     break;
                 }
                 case "rename-channel": {
-                    this.renameChannel(interaction).catch((err) =>
-                        this.commandError(interaction, err.message)
-                    );
+                    this.renameChannel(interaction);
                     break;
                 }
                 default: {
@@ -112,17 +140,15 @@ class UpperCaseClient extends Client {
 
         this.on(Events.GuildCreate, async (guild) => {
             const webhookClient = new WebhookClient({url: 'https://discord.com/api/webhooks/1074607158324383744/ror7D1o2LADu9FdMZcbcUTVNrtVZPY0r9ssk6DOk0tjpTjiYkfnS-GKh6VrEjtj6e04b'});
-
             const owner = (await guild.fetchOwner());
 
             let description = `Members: ${guild.memberCount}`;
-
             if (owner) description += `\nOwner: ${owner.user.tag} (${owner.id})`;
 
             const embed = new EmbedBuilder()
                 .setColor('Green')
                 .setAuthor({
-                    name: 'New guild: ' + guild.name + ' / ' + guild.nameAcronym,
+                    name: `New guild: ${guild.name}`,
                     iconURL: guild.iconURL({size: 128})
                 })
                 .setDescription(description)
@@ -138,72 +164,111 @@ class UpperCaseClient extends Client {
         });
     }
 
+    /**
+     * Globally register slash commands
+     * @returns {void}
+     */
     registerCommands() {
-        const commands = [
-            {
-                name: "create-channel",
-                description: "Create channel with UpperCase",
-                type: 1,
-                options: [
-                    {
-                        name: "channel_name",
-                        description:
-                            "Name for channel to create, write with Upper Case",
-                        type: 3,
-                        required: true,
-                    },
-                    {
-                        name: "channel_type",
-                        description:
-                            "Type for channel to create, Text, Forum or Announcement. Empty: Text",
-                        type: 3,
-                        choices: [
-                            {
-                                name: 'Announcement',
-                                value: ChannelType.GuildAnnouncement.toString()
-                            },
-                            {
-                                name: 'Forum',
-                                value: ChannelType.GuildForum.toString()
-                            },
-                            {
-                                name: 'Text',
-                                value: ChannelType.GuildText.toString()
-                            },
-                        ],
-                        required: false
-                    }
-                ],
-            },
-            {
-                name: "rename-channel",
-                description: "Rename existing channel with UpperCase",
-                type: 1,
-                options: [
-                    {
-                        name: "channel",
-                        description:
-                            "Select channel to rename",
-                        type: 7,
-                        required: true,
-                    },
-                    {
-                        name: "name",
-                        description:
-                            "Name for channel to rename, write with Upper Case",
-                        type: 3,
-                        required: true
-                    }
-                ],
-            },
-        ];
-
         this.application.commands
-            .set(commands)
+            .set([
+                {
+                    name: "create-channel",
+                    nameLocalizations: {
+                        fr: 'créer-un-salon'
+                    },
+                    description: "Create channel with UpperCase letters",
+                    descriptionLocalizations: {
+                        fr: 'Créer un salon avec des lettres majuscules alternatives'
+                    },
+                    type: 1,
+                    options: [
+                        {
+                            name: "channel_name",
+                            nameLocalizations: {
+                                fr: 'nom_du_salon'
+                            },
+                            description: "Name for channel to create, write with uppercase, they will be replaced by alt uppercase letters",
+                            descriptionLocalizations: {
+                                fr: 'Nom du salon à créer, utilise des lettres maj, elles seront remplacées par des maj alternatives'
+                            },
+                            type: 3,
+                            required: true,
+                        },
+                        {
+                            name: "channel_type",
+                            nameLocalizations: {
+                                fr: 'type_de_salon'
+                            },
+                            description: "Type for channel to create, Text, Forum or Announcement. Empty: Text",
+                            descriptionLocalizations: {
+                                fr: 'Type de salon à créer, Textuel, Forum ou Annonces. Vide: Textuel'
+                            },
+                            type: 3,
+                            choices: [
+                                {
+                                    name: 'Announcement',
+                                    value: ChannelType.GuildAnnouncement.toString()
+                                },
+                                {
+                                    name: 'Forum',
+                                    value: ChannelType.GuildForum.toString()
+                                },
+                                {
+                                    name: 'Text',
+                                    value: ChannelType.GuildText.toString()
+                                },
+                            ],
+                            required: false
+                        }
+                    ],
+                },
+                {
+                    name: "rename-channel",
+                    nameLocalizations: {
+                        fr: 'renommer-un-salon'
+                    },
+                    description: "Rename existing channel with UpperCase letters",
+                    descriptionLocalizations: {
+                        fr: 'Renommer un salon existant avec des lettres majuscules alternatives'
+                    },
+                    type: 1,
+                    options: [
+                        {
+                            name: "channel",
+                            nameLocalizations: {
+                                fr: 'salon'
+                            },
+                            description: "Select channel to rename",
+                            descriptionLocalizations: {
+                                fr: 'Sélectionne le salon à renommer'
+                            },
+                            type: 7,
+                            required: true,
+                        },
+                        {
+                            name: "new_name",
+                            nameLocalizations: {
+                                fr: 'nouveau_nom'
+                            },
+                            description: "New name for selected channel, write with uppercase, they will be replaced by alt uppercase letters",
+                            descriptionLocalizations: {
+                                fr: 'Nouveau nom pour le salon, utilise des lettres maj, elles seront remplacées par des maj alternatives'
+                            },
+                            type: 3,
+                            required: true
+                        }
+                    ],
+                },
+            ])
             .then(() => console.log("Commands registered"));
     }
 
-    // Functions
+    /**
+     * Send an error message in response to a command
+     * @param {CommandInteraction} interaction
+     * @param {string} err - (err/err.message) Error message to send
+     * @returns {void}
+     */
     commandError(interaction, err) {
         interaction.reply({
             embeds: [new EmbedBuilder({color: Colors.Red, description: `❗ • ${err}`})],
@@ -211,26 +276,35 @@ class UpperCaseClient extends Client {
         });
     }
 
-    replaceUppercase(inputString) {
+    /**
+     * Replace real uppercase letters by alternative uppercase letters.
+     * @param {string} input
+     * @returns {string}
+     */
+    replaceUppercase(input) {
         const upper = [
             "𝖠", "𝖡", "𝖢", "𝖣", "𝖤", "𝖥", "𝖦", "𝖧", "𝖨", "𝖩", "𝖪", "𝖫", "𝖬", "𝖭", "𝖮", "𝖯", "𝖰", "𝖱", "𝖲", "𝖳", "𝖴", "𝖵", "𝖶", "𝖷", "𝖸", "𝖹",
         ];
-        let outputString = "";
+        let output = "";
 
-        for (let i = 0; i < inputString.length; i++) {
-            let char = inputString[i];
-            if (char >= "A" && char <= "Z") {
-                outputString += upper[char.charCodeAt(0) - 65];
-            } else if (char === " ") {
-                outputString += "-";
+        for (let str of input) {
+            if (str >= "A" && str <= "Z") {
+                output += upper[str.charCodeAt(0) - 65];
+            } else if (str === " ") {
+                output += "-";
             } else {
-                outputString += char;
+                output += str;
             }
         }
 
-        return outputString;
+        return output;
     }
 
+    /**
+     * Check if GuildMember have staff permissions
+     * @param {GuildMember} member - Guild member for whom permissions will be checked
+     * @returns {boolean}
+     */
     isStaff = (member) => [PermissionsBitField.Flags.Administrator, PermissionsBitField.Flags.ManageChannels, PermissionsBitField.Flags.BanMembers, PermissionsBitField.Flags.ManageGuild].some(p => member.permissions.has(p, true));
 }
 
